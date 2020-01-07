cf91a585359eb2846ce04b4873f82194c7c5fdb5
Dawit Abera <dawit.y.abera@nordstrom.com>
2020-01-06 20:19:47 -0800

    auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,23 @@
 const JSONWebToken = require('jsonwebtoken');
-const config = require('config')
+const config = require('config');
 
-module.exports = function(req, res, next){
-    
-}
+module.exports = function(req, res, next) {
+  const token = req.header('x-auth-token');
+  if (!token) {
+    return res.status(401).json({
+      status: false,
+      message: 'No token, authorization denied'
+    });
+  }
+  try {
+    const decoded = JSONWebToken.verify(token, config.get('JWT_SECRET'));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    console.error(err);
+    return res.status(401).json({
+      status: false,
+      message: 'Token is not vaild'
+    });
+  }
+};

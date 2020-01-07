67fff874b11d3cf669125c5d4ed4fbb4b8fc4717
Dawit Abera <dawit.y.abera@nordstrom.com>
2020-01-06 21:17:34 -0800

    Profile schema created

---
+++ models/Profile.js
@@ -0,0 +1,116 @@
+/**
+ **  User Proiel Schema
+ *
+ */
+const mongoose = require('mongoose');
+
+const UserProfileSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  website: {
+    type: String
+  },
+  skills: {
+    type: [String],
+    required: [true, 'Please add skill sets']
+  },
+  status: {
+    type: String,
+    required: [true, 'Please add a status']
+  },
+  company: {
+    type: String
+  },
+  bio: {
+    type: Date
+  },
+  githubusername: {
+    type: Date
+  },
+  nocstatus: {
+    type: String,
+    enum: ['E1', 'Ninja', 'PM'],
+    default: 'E1'
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  sociallinks: {
+    twitter: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    github: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Profile', UserProfileSchema);

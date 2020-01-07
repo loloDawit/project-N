c03cf1625816b28027944071ec4536718ceaeb0a
Dawit Abera <dawit.y.abera@nordstrom.com>
2020-01-07 12:28:53 -0800

    Created posts schema

---
+++ models/Posts.js
@@ -0,0 +1,53 @@
+/**
+ **  Post Profile Schema
+ *
+ */
+const mongoose = require('mongoose');
+
+const UserPostSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  name: {
+    type: String
+  },
+  profilePic: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      profilePic: {
+        type: String
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+module.exports = Post = mongoose.model('post', UserPostSchema);

c65790ff568624a542814f5bbe552b6ace3518ca
Dawit Abera <dawit.y.abera@nordstrom.com>
2020-01-06 18:33:13 -0800

    Created mongodb connection

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const database = config.get('mongoURI');
+
+const connectDataBase = async () => {
+  try {
+    const connection = await mongoose.connect(database, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
+    console.log(`MongoDB Successfully Connected.....${connection.connection.host}`.blue.bold);
+  } catch (error) {
+    console.error();
+    process.exit(1);
+  }
+};
+module.exports = connectDataBase;

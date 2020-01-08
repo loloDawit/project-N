4d811446ef5b14c72ccbaa644c726d97ba8ed530
Dawit Abera <dawit.y.abera@nordstrom.com>
2020-01-08 13:01:50 -0800

    added more constants

--- frontend/src/actions/constant.js
+++ frontend/src/actions/constant.js
@@ -2,3 +2,7 @@ export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_FAILER = 'AUTH_FAILER';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILER = 'LOGIN_FAILER';

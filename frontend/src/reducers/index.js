d60b58adc563cb6683e8da378f48309c2525667e
Dawit Abera <dawit.y.abera@nordstrom.com>
2020-01-08 13:03:20 -0800

    added to root reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,4 +1,4 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
-
-export default combineReducers({ alert });
+import auth from './auth';
+export default combineReducers({ alert, auth });

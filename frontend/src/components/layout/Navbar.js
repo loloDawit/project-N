ceded1bd8ded43f0785e8af982685a90def90803
Dawit Abera <dawit.y.abera@nordstrom.com>
2020-01-08 13:28:58 -0800

    added auth and unauth routes for for logedin user

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-
-const Navbar = () => {
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout, loadUser } from '../../actions/auth';
+const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
+  const authrizedLinks = (
+    <ul>
+      <li>
+        <a onClick={logout} href="#!">
+          <i className="fas fa-sign-out-alt" />{' '}
+          <span className="hide-sm"> Logout </span>
+        </a>
+      </li>
+    </ul>
+  );
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to="#!">Teams</Link>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </ul>
+  );
   return (
     <nav className="navbar bg-dark">
       <h1>
@@ -9,18 +34,17 @@ const Navbar = () => {
           <i className="fas fa-address-book"></i> NOC TeamApp
         </Link>
       </h1>
-      <ul>
-        <li>
-          <Link to="!#">Teams</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authrizedLinks : guestLinks}</Fragment>
+      )}
     </nav>
   );
 };
-export default Navbar;
+Navbar.prototype = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+const mapStateToPros = state => ({
+  auth: state.auth
+});
+export default connect(mapStateToPros, { logout })(Navbar);

cece4547522b1ae158953626f7afaf763f390f75
Dawit Abera <dawit.y.abera@nordstrom.com>
2020-01-07 17:32:38 -0800

    added new fonts and created layouts

---
+++ frontend/src/components/layout/Landing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">NOC Team App</h1>
+          <p className="lead">
+            Create a developer profile/portfolio, share posts and get help from
+            other team members
+          </p>
+          <div className="buttons">
+            <a href="register.html" className="btn btn-primary">
+              Sign Up
+            </a>
+            <a href="login.html" className="btn btn-light">
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Landing;

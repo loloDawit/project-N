1cf1cd8e229c5227ab34c885b300272cf6e418b3
Dawit Abera <dawit.y.abera@nordstrom.com>
2020-01-09 13:36:09 -0800

    fixed typo

--- frontend/src/reducers/profile.js
+++ frontend/src/reducers/profile.js
@@ -50,7 +50,7 @@ export default function(state = initialState, action) {
       return {
         ...state,
         profile: null,
-        repos: [],
+        gitrepos: [],
         loading: false
       };
     default:

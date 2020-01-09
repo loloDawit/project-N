bd7a81ac7d8f48ab5a1da15dd37fd018aaaf9a5f
Dawit Abera <dawit.y.abera@nordstrom.com>
2020-01-08 16:40:53 -0800

    added a component to create a new profile

---
+++ frontend/src/components/profiledashboard/CreateProfile.js
@@ -0,0 +1,206 @@
+import React, { useState, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const CreateProfile = props => {
+  const [formData, setFormData] = useState({
+    nocstatus: '',
+    status: '',
+    company: '',
+    website: '',
+    location: '',
+    skills: '',
+    githubusername: '',
+    bio: '',
+    twitter: '',
+    linkedin: '',
+    github: ''
+  });
+  const [displaySocialInputs, toggleSocialInputs] = useState(false);
+  const {
+    nocstatus,
+    status,
+    company,
+    website,
+    location,
+    skills,
+    githubusername,
+    bio,
+    twitter,
+    linkedin,
+    github
+  } = formData;
+  const onChange = e =>
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Create Your Profile</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Let's get some information about you to
+        create your profile
+      </p>
+      <small>* = required field</small>
+      <form className="form">
+        <div className="form-group">
+          <select name="status" value={status} onChange={e => onChange(e)}>
+            <option value="0">* Select Professional Status</option>
+            <option value="Developer">Developer</option>
+            <option value="Junior Developer">Junior Developer</option>
+            <option value="Senior Developer">Senior Developer</option>
+            <option value="Manager">Manager</option>
+            <option value="Student or Learning">DevOps</option>
+            <option value="Instructor">Network Engineer</option>
+            <option value="Intern">Sinor Manager</option>
+            <option value="Other">Other</option>
+          </select>
+          <small className="form-text">
+            Give us an idea of where you are at in your career
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Company"
+            name="company"
+            value={company}
+            onChange={e => onChange(e)}
+          />
+          <small className="form-text">
+            Could be your own company or one you work for
+          </small>
+        </div>
+        <div className="form-group">
+          <select
+            name="nocstatus"
+            value={nocstatus}
+            onChange={e => onChange(e)}
+          >
+            <option value="0"> Select NOC Status</option>
+            <option value="E1">Enginner 1</option>
+            <option value="Ninja">Ninja</option>
+            <option value="PM">PM</option>
+          </select>
+          <small className="form-text">Give us your NOC status</small>
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Website"
+            name="website"
+            value={website}
+            onChange={e => onChange(e)}
+          />
+          <small className="form-text">
+            Could be your own or a company website
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Location"
+            name="location"
+            value={localStorage}
+            onChange={e => onChange(e)}
+          />
+          <small className="form-text">
+            City & state suggested (eg. Seattle, WA)
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* Skills"
+            name="skills"
+            value={skills}
+            onChange={e => onChange(e)}
+          />
+          <small className="form-text">
+            Please use comma separated values (eg.
+            Network,Operations,JavaScript,Database)
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Github Username"
+            name="githubusername"
+            value={githubusername}
+            onChange={e => onChange(e)}
+          />
+          <small className="form-text">
+            If you want your latest repos and a Github link, include your
+            username
+          </small>
+        </div>
+        <div className="form-group">
+          <textarea
+            placeholder="A short bio of yourself"
+            name="bio"
+            value={bio}
+            onChange={e => onChange(e)}
+          ></textarea>
+          <small className="form-text">Tell us a little about yourself</small>
+        </div>
+
+        <div className="my-2">
+          <button
+            onClick={() => {
+              toggleSocialInputs(!displaySocialInputs);
+            }}
+            type="button"
+            className="btn btn-light"
+          >
+            Add Social Network Links
+          </button>
+          <span>Optional</span>
+        </div>
+
+        {displaySocialInputs && (
+          <Fragment>
+            {' '}
+            <div className="form-group social-input">
+              <i className="fab fa-twitter fa-2x"></i>
+              <input
+                type="text"
+                placeholder="Twitter URL"
+                name="twitter"
+                value={twitter}
+              />
+            </div>
+            <div className="form-group social-input">
+              <i className="fab fa-github fa-2x"></i>
+              <input
+                type="text"
+                placeholder="GitHub URL"
+                name="GitHub"
+                value={twitter}
+              />
+            </div>
+            <div className="form-group social-input">
+              <i className="fab fa-linkedin fa-2x"></i>
+              <input
+                type="text"
+                placeholder="Linkedin URL"
+                name="linkedin"
+                value={linkedin}
+                onChange={e => onChange(e)}
+              />
+            </div>{' '}
+          </Fragment>
+        )}
+
+        <input type="submit" className="btn btn-primary my-1" />
+        <a className="btn btn-light my-1" href="dashboard.html">
+          Go Back
+        </a>
+      </form>
+    </Fragment>
+  );
+};
+
+CreateProfile.propTypes = {};
+
+export default connect()(CreateProfile);

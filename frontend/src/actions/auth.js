0f77cc8e3f8d958e8f1a0a1406275bea4835ca94
Dawit Abera <dawit.y.abera@nordstrom.com>
2020-01-08 19:27:50 -0800

    removed log

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -84,8 +84,6 @@ export const login = (email, password) => async dispatch => {
     dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
-    console.log(errors);
-
     if (errors) {
       errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
     }

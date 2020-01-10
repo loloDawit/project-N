f2997692cf20e413fb5ad1fbccd655abb8f4a321
Dawit Abera <dawit.y.abera@nordstrom.com>
2020-01-10 15:03:31 -0800

    added actions to add and remove likes

--- frontend/src/actions/post.js
+++ frontend/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_FAILER } from './constant';
+import { GET_POSTS, POST_FAILER, POST_UPDATE_LIKES } from './constant';
 
 export const getUserPosts = () => async dispatch => {
   try {
@@ -8,6 +8,42 @@ export const getUserPosts = () => async dispatch => {
     dispatch({
       type: GET_POSTS,
       payload: res.data.data
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_FAILER,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+/**
+ *
+ * @param {*} post_id Id of the post object
+ */
+export const addLikeToUserPosts = post_id => async dispatch => {
+  try {
+    const res = await axios.put(`/api/v1/posts/like/${post_id}`);
+    dispatch({
+      type: POST_UPDATE_LIKES,
+      payload: { post_id, likes: res.data.data }
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_FAILER,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+/**
+ *
+ * @param {*} post_id Id of the post object
+ */
+export const removeLikeFromUserPosts = post_id => async dispatch => {
+  try {
+    const res = await axios.put(`/api/v1/posts/unlike/${post_id}`);
+    dispatch({
+      type: POST_UPDATE_LIKES,
+      payload: { post_id, likes: res.data.data }
     });
   } catch (err) {
     dispatch({

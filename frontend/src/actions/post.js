c8056b3849f3646e9858162ed7794b7b28f7b6e9
Dawit Abera <dawit.y.abera@nordstrom.com>
2020-01-10 16:31:45 -0800

    added delete func to delete a post

--- frontend/src/actions/post.js
+++ frontend/src/actions/post.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_FAILER, POST_UPDATE_LIKES } from './constant';
+import {
+  GET_POSTS,
+  POST_FAILER,
+  POST_UPDATE_LIKES,
+  DELETE_POST
+} from './constant';
 
 export const getUserPosts = () => async dispatch => {
   try {
@@ -45,6 +50,25 @@ export const removeLikeFromUserPosts = post_id => async dispatch => {
       type: POST_UPDATE_LIKES,
       payload: { post_id, likes: res.data.data }
     });
+  } catch (err) {
+    dispatch({
+      type: POST_FAILER,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+/**
+ *
+ * @param {*} post_id Id of the post object
+ */
+export const deletePostFromUserPosts = post_id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/v1/posts/${post_id}`);
+    dispatch({
+      type: POST_UPDATE_LIKES,
+      payload: { post_id, likes: res.data.data }
+    });
+    dispatch(setAlert('Post Deleted', 'success'));
   } catch (err) {
     dispatch({
       type: POST_FAILER,

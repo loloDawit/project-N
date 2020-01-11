487a4940b6ce64b7bf8e089d99d48eea5ba2a1b7
Dawit Abera <dawit.y.abera@nordstrom.com>
2020-01-10 16:46:02 -0800

    added addpost to action reducer

--- frontend/src/reducers/post.js
+++ frontend/src/reducers/post.js
@@ -2,7 +2,8 @@ import {
   GET_POSTS,
   POST_FAILER,
   POST_UPDATE_LIKES,
-  DELETE_POST
+  DELETE_POST,
+  ADD_POST
 } from '../actions/constant';
 
 const initialState = {
@@ -19,6 +20,12 @@ export default function(state = initialState, action) {
       return {
         ...state,
         posts: payload,
+        loading: false
+      };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [...state.posts, payload],
         loading: false
       };
     case POST_FAILER:

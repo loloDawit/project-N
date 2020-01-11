79cd1d98af6a48ddb8ab8383962ca240f947e7ae
Dawit Abera <dawit.y.abera@nordstrom.com>
2020-01-10 16:43:16 -0800

    added addpost to the action

--- frontend/src/actions/post.js
+++ frontend/src/actions/post.js
@@ -4,7 +4,7 @@ import {
   GET_POSTS,
   POST_FAILER,
   POST_UPDATE_LIKES,
-  DELETE_POST
+  DELETE_POST, ADD_POST
 } from './constant';
 
 export const getUserPosts = () => async dispatch => {
@@ -63,12 +63,36 @@ export const removeLikeFromUserPosts = post_id => async dispatch => {
  */
 export const deletePostFromUserPosts = post_id => async dispatch => {
   try {
-    const res = await axios.delete(`/api/v1/posts/${post_id}`);
+    await axios.delete(`/api/v1/posts/${post_id}`);
     dispatch({
-      type: POST_UPDATE_LIKES,
-      payload: { post_id, likes: res.data.data }
+      type: DELETE_POST,
+      payload: post_id
     });
     dispatch(setAlert('Post Deleted', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_FAILER,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+/**
+ *
+ * @param {*} post_id Id of the post object
+ */
+export const addPostFromUserPosts = formData => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  try {
+    const res = await axios.post('/api/v1/posts/', formData, config);
+    dispatch({
+      type: ADD_POST,
+      payload: res.data.data
+    });
+    dispatch(setAlert('Post Added', 'success'));
   } catch (err) {
     dispatch({
       type: POST_FAILER,
